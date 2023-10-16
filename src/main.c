f7b001c6ffc9cb39d9438afee5f4c23f5cd2a6d0
hdlmx <hdlmx@outlook.com>
2023-10-16 22:42:56 +0800

    链表

--- src/main.c
+++ src/main.c
@@ -6,8 +6,39 @@
 //
 
 #include <stdio.h>
-int main(int argc, const char * argv[]) {
-    // insert code here...
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    /* data */
+};
+
+int main(int argc, const char *argv[])
+{
+
+    struct Node head = {0, NULL};
+
+    struct Node *point = &head;
+
+    for (int i = 1; i < 3; i++)
+    {
+        struct Node *node = malloc(sizeof(struct Node));
+        node->data = i;
+        point->next = node;
+        point = point->next;
+    }
+
+    struct Node *index = &head;
+
+    while (index->next != NULL)
+    {
+        printf("%d", index->data);
+        index = index->next;
+    }
+
     printf("Hello, hdlmx!\n");
     return 0;
 }
